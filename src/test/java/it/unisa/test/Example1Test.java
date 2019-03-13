8ff4a78facf10e4cf1edc02f5163b5030cd8aa8b
StefanoLambiase <stefanolambiase96@gmail.com>
2019-03-13 16:11:07 +0100

    Commit di test

--- src/test/java/it/unisa/test/Example1Test.java
+++ src/test/java/it/unisa/test/Example1Test.java
@@ -1,33 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.test;
 
-import it.unisa.progetto.Example1;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- *
- * @author Rembor
- */
-public class Example1Test{
-    @Test
-    public void test1() {
-        new Example1().doSomething(1);
+import static org.junit.Assert.*;
+
+public class Example1Test {
+    private String testString;
+
+    @Before
+    public void setUp() throws Exception {
+        testString = "miaow";
     }
 
     @Test
-    public void test2() {
-        new Example1().doSomething(2);
-        int a = 4;
-        assert(a > 2);
+    public void doSomething() {
     }
-    
-     @Test
-    public void test3(){
-        new Example1().doSomething(22);
-    }
-
 }
